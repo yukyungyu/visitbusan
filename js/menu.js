ce11d26859c06e7df9a453a209f2b7e3436ca436
Cho-s-y <tlsdud8540@naver.com>
2023-09-25 15:34:25 +0900

    menu 추가

---
+++ js/menu.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+  $(window).resize(function(){
+    let menuHeight = $(".menu_in").height();
+
+    if(menuHeight < 500){
+      $(util).hide();
+    }else{
+      $(util).show();
+    }
+  });
+
+  $(".gnb .gnb_in").click(function(){
+    $(this).find(".sub").stop().slideToggle("slow");
+    $(this).siblings().find(".sub").stop().slideUp();
+    $(this).toggleClass("active");
+    $(this).siblings().removeClass("active");
+  });
+
+  $(".menu").click(function(){
+    $(".menu_in").css({zIndex:"1000"}).stop().animate({height:"100%",opacity:"1"},800);
+    $(".dockbar").stop().fadeOut("fast");
+  });
+  $(".close").click(function(){
+    $(".menu_in").stop().animate({height:"0%",opacity:"0"},800);
+    $(".dockbar").stop().fadeIn(1400);
+  });
+  
+
+  $(".bottom_menu li").each(function(){
+    let nowImg = $(this).find("img");
+    let srcName = nowImg.attr("src");
+    let newSrc = srcName.substring(0, srcName.lastIndexOf('.'));
+
+    $(this).hover(function(){
+      nowImg.attr('src', newSrc + '_hover.' + /[^.]+$/.exec(srcName));
+    }, function(){
+      nowImg.attr('src', newSrc + '.' + /[^.]+$/.exec(srcName));
+    });
+  });
+
+});
